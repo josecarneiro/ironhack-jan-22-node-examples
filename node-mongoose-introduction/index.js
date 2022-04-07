719c5f92d30234799c9bed9b0112ac9669e0f634
José Carneiro <contact@josecarnei.ro>
2022-04-07 20:30:53 +0100

    Add library example

--- node-mongoose-introduction/index.js
+++ node-mongoose-introduction/index.js
@@ -13,8 +13,16 @@ const companySchema = new mongoose.Schema();
 // you will have a collection in your database
 // Model name
 // equals collection name in singular form
-// Models should be capitalized,
-// even when collection names are lowercase (or camelCase)
+
+// Models should be capitalized and singular,
+// even though collection names are lowercase (or camelCase) and plural
+// Mongoose will coerce the model name from PascalCase to camelCase
+// and it will pluralize it
+// Model name "Company" becomes collection name "companies"
+
+// 'Company' is the name for the model
+// companySchema refers to this Model's schema
+// (it is an object instantiated from the mongoose.Schema class)
 const Company = mongoose.model('Company', companySchema);
 
 mongoose
@@ -41,3 +49,54 @@ mongoose
   .catch((error) => {
     console.log('There was an error connecting to mongodb', error);
   });
+
+// Calling mongoose.model('ModelName', modelSchema) "creates a class"
+// this class can be instantiated
+// any methods of the class can be called on objects instantiated from that class
+
+// Calling Model.find() issues a query to the mongoDB database
+// which is the application is connected to, it returns a promise
+// that resolves once mongoDB has returned a result for this
+// asynchronous query
+// This promise will resolve with a list of documents that match the filter
+// object passed to the find method
+
+// Static methods of mongoose models
+// find() -> Query database for many results
+// findOne() -> Query database for single result
+// findById() -> Query database for single result that matches a certain id
+// create() -> Add document to collection
+// findOneAndUpdate -> Update single document
+// findByIdAndUpdate -> Update single document that a matches a certain id
+// findOneAndDelete -> Delete single document
+// findByIdAndDelete -> Delete single document that a matches a certain id
+// exists -> Resolves with true if document that matches condition exists
+// counts -> Resolves with number of documents that match condition
+
+// Modifier static methods of mongoose models
+// can be chained on other static methods
+// limit() -> Limits count of documents returned
+// select() -> Project fields (choose to include or exclude fields)
+// sort() -> Sorts documents
+// skip() -> Skip over documents
+// populate() -> To be discussed
+
+// Company.find().skip(10).limit(5).sort({ founded_year: 1 }).select('name');
+// Return a promise that resolves with 5 documents skipping over the first 10,
+// sorted by founded year. Documents will only hold _id and name property.
+
+/*
+class Dog {
+  // bark would be called on an instance of the Dog class
+  bark() {
+    console.log('WOOF');
+  }
+  // foo would be called on the Dog class itself
+  static foo() {
+    console.log('BAR');
+  }
+}
+const dog = new Dog();
+dog.bark();
+Dog.foo();
+*/
---
+++ node-mongoose-introduction/library.js
@@ -0,0 +1,118 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      // Every book document must have a value for 'available'
+      required: true, // Required is not a validator (it's a bit more complex)
+      minlength: 3, // minimum length validator
+      maxlength: 140 // maximum length validator
+    },
+    pages: {
+      type: Number,
+      min: 1, // minimum value validator
+      max: 10000 // maximum value validator
+    },
+    available: {
+      type: Boolean,
+      // We're saying that by default every document will have
+      // 'available' set to true unless specified otherwise
+      default: true
+    },
+    // genres: [String],
+    genres: [
+      {
+        type: String,
+        enum: ['science', 'education', 'romance', 'drama', 'fantasy']
+      }
+    ],
+    condition: {
+      type: String,
+      // We can specify that 'condition' can only have one of two values
+      // either 'new' or 'used'
+      enum: ['new', 'used'] // Schema validator
+    },
+    publicationDate: {
+      type: Date
+    }
+    // Make every document have a createdAt property
+    // which holds the creation date
+    /*
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+  }
+  */
+  },
+  // With this options object,
+  // we can tell mongoose to always include createdAt and updatedAt
+  // values in our documents
+  { timestamps: true }
+);
+
+const Book = mongoose.model('Book', bookSchema);
+
+mongoose
+  .connect('mongodb://localhost:27017/library')
+  .then(() => {
+    console.log('Connected to mongodb');
+    // To add single document to books collection,
+    // we call the Book.create() static method
+    // and pass it an object with properties and values
+    // that this specific book document should have
+    return Book.create({
+      name: 'Sapiens',
+      pages: 498,
+      available: true,
+      condition: 'new',
+      genres: ['science', 'education'],
+      publicationDate: new Date(2011, 9, 1)
+    });
+  })
+  .then((book) => {
+    console.log('Created a book', book);
+    // We can call Book.create() and pass it an array of objects
+    // to have multiple documents inserted into the collection at once
+    return Book.create([
+      { name: 'The Secret Life of Planets', pages: 274 },
+      { name: 'The Master Algorithm', pages: 347 }
+    ]);
+  })
+  .then((books) => {
+    console.log('Multiple books added', books);
+    return Book.find({ pages: { $gte: 300 } });
+  })
+  .then((books) => {
+    console.log('Books with more than 300 pages', books);
+    return Book.findOne({ available: true });
+  })
+  .then((book) => {
+    console.log('This is one of the available books', book);
+    // Note: if no books had 'available' set to true,
+    // the parameter book would hold value null
+    return Book.findOneAndUpdate(
+      { pages: { $lte: 300 }, available: true },
+      { available: false },
+      { new: true } // Forces mongoose to resolve promise with up to date document
+    );
+  })
+  .then((book) => {
+    console.log('Book was updated', book);
+    return Book.findOneAndDelete({ name: 'Sapiens' });
+  })
+  .then(() => {
+    console.log('Book was deleted');
+    // Instead of a filter object, these take a document's id
+    // and behave the same as findOne, findOneAndUpdate and findOneAndDelete respectively
+    // findById
+    // findByIdAndUpdate
+    // findByIdAndDelete
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log('Was disconnected from MongoDB');
+  })
+  .catch((error) => {
+    console.log('There was an error connecting to mongodb', error);
+  });
